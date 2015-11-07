ee63d9cfa387200b4f3249a7b9bd71e24beb3374
rcorbeil <rcorbeil@softstackfactory.com>
2015-11-07 01:05:45 +0000

    working ballots

--- www/js/RESTServices.js
+++ www/js/RESTServices.js
@@ -46,31 +46,124 @@ function ($http,  ENDPOINT_URL) {
   };
 
 }])
-.service('ServerAnswersService', ['$http', 'ENDPOINT_URL', 
+.service('ServerBallotService', ['$http', 'ENDPOINT_URL', 
 function ($http, ENDPOINT_URL) {
   var service = this,
-  path = 'TestResults/';
+  path = 'Ballots/';
 
   function getUrl() {
     return ENDPOINT_URL + path;
   }
 
-  service.create = function(answer, token) {
+  service.create = function(ballot, token) {
     return $http({
         url: getUrl(),
         method: "POST",
-        data: JSON.stringify(answer),
+        data: ballot,
         headers: {
             'Authorization': token
         }
      });
   };
   
+  service.getLatestBallot = function(userID, token)
+  {
+    return $http.get(getUrl()+"?filter[where][voterId]="+userID+"&filter[order]=updated DESC&filter[limit]=1",{
+        params: { access_token: token }
+    });
+  };
+  
   service.all = function(userID, token)
   {
-    return $http.get(getUrl()+"?filter[where][userID]="+userID,{
+    return $http.get(getUrl()+"?filter[where][voterId]="+userID,{
         params: { access_token: token }
     });
   };
 
-}]);
+}])
+
+.service('ServerCurrentTotalsService', ['$http', 'ENDPOINT_URL', 
+function ($http, ENDPOINT_URL) {
+  var service = this,
+  path = 'CurrentTotals/';
+
+  function getUrl() {
+    return ENDPOINT_URL + path;
+  }
+
+  service.upvoteFirst = function(candidateId, token) {
+    return $http({
+        url: getUrl()+"upvoteFirst",
+        method: "POST",
+        headers: {
+            'Authorization': token,
+            'id': candidateId
+        }
+     });
+  };
+  
+  service.upvoteSecond = function(candidateId, token) {
+    return $http({
+        url: getUrl()+"upvoteSecond",
+        method: "POST",
+        headers: {
+            'Authorization': token,
+            'id': candidateId
+        }
+     });
+  };
+  
+  service.upvoteThird = function(candidateId, token) {
+    return $http({
+        url: getUrl()+"upvoteThird",
+        method: "POST",
+        headers: {
+            'Authorization': token,
+            'id': candidateId
+        }
+     });
+  };
+  
+  service.downvoteFirst = function(candidateId, token) {
+    return $http({
+        url: getUrl()+"downvoteFirst",
+        method: "POST",
+        headers: {
+            'Authorization': token,
+            'id': candidateId
+        }
+     });
+  };
+  
+  service.downvoteSecond = function(candidateId, token) {
+    return $http({
+        url: getUrl()+"downvoteSecond",
+        method: "POST",
+        headers: {
+            'Authorization': token,
+            'id': candidateId
+        }
+     });
+  };
+  
+  service.downvoteThird = function(candidateId, token) {
+    return $http({
+        url: getUrl()+"downvoteThird",
+        method: "POST",
+        headers: {
+            'Authorization': token,
+            'id': candidateId
+        }
+     });
+  };
+  
+  service.getCurrentTotal = function(candidateId, token)
+  {
+    return $http.get(getUrl()+"findOne/?filter[where][candidateId]="+candidateId+"&filter[order]=date DESC",{
+        params: { access_token: token }
+    });
+  };
+              
+
+}])
+;
